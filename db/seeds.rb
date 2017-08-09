30d548de2a538df9529d818a65393b0cc9c944e5
Judith Hsiang <judithyhh@gmail.com>
2017-08-09 14:32:27 +0800

    Models added.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: 'Salad Nest', address: "Tianfu Shingu", category: "chinese")
+Restaurant.create(name: 'Pancake Dao', address: "New Port", category: "italian")
+Restaurant.create(name: 'Super Sushi', address: "Tokyo City", category: "japanese")
+Restaurant.create(name: 'Fun French', address: "Paris Blvd", category: "french")
+Restaurant.create(name: 'Waffles Extraordinaire', address: 'Europe Highway', category: "belgian")
+
